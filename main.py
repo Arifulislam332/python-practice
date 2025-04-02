b9dbcd1504972700b35bde54c808fcf8b068e7f9
Ayatullah Arif <ayatullaharif332@gmail.com>
2025-04-02 22:39:15 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,108 @@
+"""# this is the X value.
+x=5
+# this is the Y value.
+y=2
+# summation
+sum=x+y
+# subtraction
+sub=y-x
+# multiplication
+# mul=x*y
+# division
+divi=x/y"""
+
+""""
+hello bangladesh!
+"""
+
+"""print(sum)
+print(sub)
+print(mul)
+print(divi)"""
+
+# country_name="Bangladesh"
+
+# print(country_name)
+
+# _my_name="Ayatullah Arif"
+
+# print(_my_name)
+
+# MY_EMAIL_ID='ayatullaharif332@gmail.com'
+# print(MY_EMAIL_ID)
+
+# DATA TYPE
+"""# integer data type
+telephone= 1936711314
+# print(telephone)
+roll_number=56
+# print(roll_number)
+
+result=55.5
+# print(result)"""
+
+"""international_country=["denmark","australia","japan","china"]
+
+print(international_country)
+print(international_country[3])"""
+
+"""mul_data=[2,"arif",2.45,55,"bangladesh","XIII"]
+
+print(mul_data[5])
+
+dabble__data=[2,2,3,3,"china","thailand","china"]
+
+print(dabble__data[5])
+"""
+
+complex_data=2.2+4j
+
+print(complex_data)
+
+
+"""players=("naymar","messi","ronaldo",'martinij')
+
+print(*players)
+"""
+
+"""numbers=range(1,13,5)
+print(*numbers)"""
+
+# BOOLEAN DATA TYPE
+
+"""isBangladeshi= True
+print(isBangladeshi)"""
+
+# NONE DATA TYPE
+"""name=None
+print(name)"""
+
+# MAPPING DATA TYPE
+# dictionary data
+"""student= {
+    'name':'Ariful Islam',
+    'roll':34,
+    'class':'XIII',
+    'isBangladeshi': True
+}"""
+
+# print(student['roll'])
+# print(student['class'])
+# print(student['isBangladeshi'])
+
+# SET - mutable
+
+"""common_str= {"Arif", "Sofik", "Rofik", "Arif",2,5,2}
+
+print(common_str)
+
+# frozenSet - immutable
+common_str=frozenset([1,2,34,1,3,43,34])
+check= type(common_str)
+print(check)
+"""
+
+"""student={
+    'name':'Arif'
+}
+print(student)"""
